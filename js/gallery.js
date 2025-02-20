5aa35a93c90b070d487b136f08995d5344743f60
room116marketing <lobby@theroom116.com>
2025-02-20 10:50:54 +0100

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -2,26 +2,25 @@
 // La selección de la carpeta que se usa para la carga se elige mediante el id del contenedor gallery-""
 // Se cargan todas las imágenes que están dentro de la carpeta, pero es necesario que las fotos estén enumeradas
 // En la carga de la web se muestran las fotos reducidas que están dentro de la carpeta "reduced", pero para la descarga se usa la imagen completa
-
 document.addEventListener('DOMContentLoaded', function () {
   const gallery = document.querySelector('.gallery');
   const loadMoreButton = document.getElementById('load-more');
-  
+
   if (gallery && loadMoreButton) {
     const folder = gallery.id.split('-')[1];
     let imgCount = 0;
     let loadedImages = 0;
-    const imagesPerLoad = 4;  
+    const imagesPerLoad = 4;
 
-    function loadImage(index) {
+    function loadImage(index, extension = 'jpg') {
       const img = new Image();
-      img.src = `img/${folder}/${index}.jpg`;
+      img.src = `img/${folder}/${index}.${extension}`;
 
       img.onload = function () {
-        createGalleryItem(index);
+        createGalleryItem(index, extension);
         loadedImages++;
         imgCount++;
-        
+
         if (loadedImages < imagesPerLoad) {
           loadImage(index + 1);
         } else {
@@ -30,45 +29,56 @@ document.addEventListener('DOMContentLoaded', function () {
       };
 
       img.onerror = function () {
-        if (loadedImages === 0) {
-          loadMoreButton.style.display = 'none';
-        } else {
-          loadMoreButton.style.display = 'block';
+        if (extension === 'jpg') {
+           loadImage(index, 'png');
+        } else if (extension === 'png') {
+       
+          if (loadedImages === 0) {
+            loadMoreButton.style.display = 'none';
+          } else {
+            loadMoreButton.style.display = 'block';
+          }
         }
       };
     }
 
-    function createGalleryItem(index) {
+    function createGalleryItem(index, extension) {
       const item = document.createElement('div');
       item.classList.add('item');
 
-      const imageSrc = `img/${folder}/reduced/${index}-thumb.jpg`;
+      const imageSrcJpg = `img/${folder}/reduced/${index}-thumb.jpg`;
+      const imageSrcPng = `img/${folder}/reduced/${index}-thumb.png`;
 
       const link = document.createElement('a');
-      link.href = imageSrc;
-      link.download = `${folder}-image-${index}.jpg`;
+      link.download = `${folder}-image-${index}.${extension}`;
 
-      const image = document.createElement('img');
-      image.src = imageSrc;
+      const image = new Image();
+      image.src = imageSrcJpg;
       image.alt = `Image ${index}`;
+
+      image.onerror = function () {
+    
+        image.src = imageSrcPng;
+      };
 
       const icon = document.createElement('img');
       icon.classList.add('galery-img-icon');
       icon.src = 'img/icon.svg';
       icon.alt = '';
 
+      link.href = `img/${folder}/${index}.${extension}`;
       link.appendChild(image);
       link.appendChild(icon);
       item.appendChild(link);
       gallery.appendChild(item);
     }
 
-    loadMoreButton.addEventListener('click', function() {
+    loadMoreButton.addEventListener('click', function () {
       loadedImages = 0;
       loadImage(imgCount + 1);
     });
 
-    // Initial load
+ 
     loadImage(1);
   }
 });
